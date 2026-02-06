817d64cdf4f0001d56dd737121849d01a9acf7e9
omar <ok.elsawy@gmail.com>
2026-02-06 17:28:34 -0500

    Fixed workflow issues

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() {
     }
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
